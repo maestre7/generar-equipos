6114cb45b40d7cf2499f7bac1761923d8ce82b7a
maestre7 <maestre7@gmail.com>
2021-10-17 23:42:51 +0200

    Validate teams

--- js/main.js
+++ js/main.js
@@ -1,2 +1,24 @@
 ﻿
 //JS
+
+let members = ["Traidor", "Desalmado", "Juampi", "Lucia", "Desde casa", "En directo", "Presencial"];
+
+function validateInputTeams() {
+	let membersTeams = document.forms["teams"]["members"].value;
+	
+	if (membersTeams < 2) {
+		alert("No puede haber menos de dos miembros por equipo");
+		return false;
+	} else if (members.length < membersTeams) {
+		alert("No tienes suficientes miembros para hacer dos equipos");
+		return false;
+	} else {
+		createTeams();
+	}	
+}
+
+
+function createTeams() {
+	let membersTeams = document.forms["teams"]["members"].value;
+	alert(`${members}`);
+}
