1b33694b9c31c45abb23a96905a29803c2a11c90
Sol <ssuarezf84@gmail.com>
2021-10-23 13:54:58 +0200

    Paso2

--- js/equipos.js
+++ js/equipos.js
@@ -1,11 +1,14 @@
 function team2() {
     let elementList = document.getElementById("name_list") // Lista
     let newElementListOne = document.createElement('li'); // Campo
-    let value_in = document.getElementById('Add_name'); // Valor in
+    let value_in = document.getElementById('Add_name').value; // Valor in
     newElementListOne.innerHTML = value_in; // Integrantes del equipo
+    listNames.push(value_in);
     elementList.appendChild(newElementListOne); // Añadir campo a lista
 }
 
 function validateAdd() {
     return true;
 }
+
+let listNames = [];
