47d571819757256a3e89baab730a0236d0a61f3c
maestre7 <maestre7@gmail.com>
2021-10-23 13:10:46 +0200

    finish a falta del array

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ let membersArray = ["Traidor", "Desalmado", "Juampi", "Lucia", "Desde casa", "En
 //let no-primera = false;
 
 // Verificamos que los miembros por equipo no sean menos de dos, ni mas que la gente disponible.
-// Si todo esta bien llamamos a la funcion que montara y mostrara los equipos
+// Si todo esta bien llamamos a la function que montara y mostrara los equipos
 function validateInputTeams() {
 
 	let membersTeams = document.forms["teams"]["members"].value;
@@ -27,7 +27,9 @@ function createTeams() {
 	let membersTeams = document.forms["teams"]["members"].value;
 
 	let myMembers = membersArray.slice();
+	let teamControl = 1;
 	
+	createGroup();
 	while (myMembers.length != 0) {
 		let teamArray = [];
 
@@ -42,8 +44,10 @@ function createTeams() {
 			}
 		}
 
-		includeMembers(teamArray);
+		includeMembers(teamArray, teamControl);
+		teamControl += 1;
 	}
+	includeLine();
 }
 
 // Retorna un número aleatorio entre min (incluido) y max (excluido)
@@ -52,23 +56,40 @@ function getRandomArbitrary(min, max) {
 }
 
 // Creamos las listas de los miembros en html y las rellenamos
-function includeMembers(teamArray) {
-	// Recuperamos el element donde mostraremos los teams
-	//if (no-primera) {
-		//let ul = document.querySelector("div.teams>ul");	
-		//ul.remove();
-	//}
-	let elementTeams = document.querySelector("div.teams");	
+function includeMembers(teamArray, teamControl) {
+
+	let elementTeams = document.querySelector("div.teams > div:last-child");	
 
 	let newElementList = document.createElement('ul');
-
+	let newElementDiv = document.createElement('div');
+	newElementDiv.innerHTML = `TEAM ${teamControl}`; // Numero de equipo
+	newElementList.appendChild(newElementDiv);
+	
 	for (let v of teamArray) {
 		let newElementListOne = document.createElement('li');
-		newElementListOne.innerHTML = v;
+		newElementListOne.innerHTML = v; // Integrantes del equipo
 		newElementList.appendChild(newElementListOne);
 	}
+	
 	elementTeams.appendChild(newElementList);
-	//no-primera = true;
+
 }
 
+// Creamos linea divisoria
+function includeLine() {
+	
+	let elementTeams = document.querySelector("div.teams");
+	let newElementLine = document.createElement('div');
+	let myAttribute = document.createAttribute("class");
+	myAttribute.value = "line";
+	newElementLine.setAttributeNode(myAttribute);
+	
+	elementTeams.appendChild(newElementLine);
+}
 
+// Añadimos un div para cada grupo de equipos
+function createGroup() {
+	let elementTeams = document.querySelector("div.teams");
+	let newElementDiv = document.createElement('div');
+	elementTeams.appendChild(newElementDiv);	
+}
